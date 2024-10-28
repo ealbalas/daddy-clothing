5f51908d62234f2ee1f0508b8d0adc1793e8830a
Emad Albalas <ealbalas3@gatech.edu>
2024-10-28 01:02:34 -0700

    added navigation bar with logo and shop link

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,22 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment} from 'react';
+import { ReactComponent as DaddyLogo } from '../../assests/daddy.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <DaddyLogo className='logo'/>
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>SHOP</Link>
+            </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    );
+}
+
+export default Navigation;
