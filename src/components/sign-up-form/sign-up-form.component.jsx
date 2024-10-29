1947ef25584c8462bf5d4209de45e701a4934e44
Emad Albalas <ealbalas3@gatech.edu>
2024-10-29 00:14:51 -0700

    centralized all code for when user sign in and sign out all in user context

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,8 +1,7 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
-import { UserContext } from '../../contexts/user.context';
 import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
@@ -15,8 +14,6 @@ const defaultFormFields = {
 const SignUpForm  = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
-
-    const { setCurrentUser } = useContext(UserContext);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -34,7 +31,6 @@ const SignUpForm  = () => {
         try {
             const { user } = await createAuthUserWithEmailAndPassword(email, password);
             await createUserDocumentFromAuth(user, {displayName });
-            setCurrentUser(user);
             resetFormFields();
         } catch(error) {
             if (error.code === 'auth/email-already-in-use') {
