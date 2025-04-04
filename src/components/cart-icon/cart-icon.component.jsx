4e91c2623cdcdd26432675a62701c8d5190578fc
Emad Albalas <emadalbalas@gmail.com>
2025-04-04 17:26:42 +0300

    modified cart dropdown to accurately reflect the items in cart. Also add count to cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -12,7 +12,7 @@ const CartIcon = () => {
     return (
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 }
