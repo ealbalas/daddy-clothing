e59f645ab5208d53a7cf78a2fcb7d472f3d1958e
Emad Albalas <emadalbalas@gmail.com>
2025-04-04 18:38:37 +0300

    created checkout page along with checkout items and checkout cart functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import { UserProvider } from './contexts/user.context';
 import { CartProvider } from './contexts/cart.context';
+import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
             <Route index element={<Home />} />
             <Route path='shop' element={<Shop />} />
             <Route path='auth' element={<Authentication />} />
+            <Route path='checkout' element={<Checkout />} />
           </Route>
         </Routes>
       </CartProvider>
